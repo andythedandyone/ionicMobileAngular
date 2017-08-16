5cdf10f8288faaa25ff13174c274944ca287a23f
DESKTOP-4F9NOBO\User <andersonlavor@gmail.com>
2017-08-15 20:44:59 -0400

    working in side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import {TabsPage} from "../pages/tabs/tabs";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  tabsPage = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
